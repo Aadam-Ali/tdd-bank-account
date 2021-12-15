92874607093202ceb647fc3858330995e0fbed17
Aadam Ali <ali.aadam@outlook.com>
2021-12-15 11:35:07 +0000

    added test for withdraw method error

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -15,5 +15,10 @@ describe BankAccount do
     expect(subject.withdraw(5)).to eq 5
   end
 
+  it 'raises and error when you attempt to withdraw an amount greater than balance' do
+    subject.deposit(10)
+    expect {subject.withdraw(15)}.to raise_error 'Not enough funds'
+  end 
+
   it { is_expected.to respond_to :balance }
 end
