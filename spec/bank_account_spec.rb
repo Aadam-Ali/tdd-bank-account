3757ebb55af7b007f0126129de8095c0e97fa5c1
Aadam Ali <ali.aadam@outlook.com>
2021-12-15 11:23:57 +0000

    updated test for deposit method

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -3,7 +3,8 @@ require 'bank_account'
 describe BankAccount do
   it { is_expected.to respond_to(:deposit).with(1).argument }
 
-  it 'deposits amount and returns amount' do
-    expect(subject.deposit(10)).to eq 10
+  it 'deposits amount and returns balance' do
+    subject.deposit(10)
+    expect(subject.balance).to eq 10
   end
 end
